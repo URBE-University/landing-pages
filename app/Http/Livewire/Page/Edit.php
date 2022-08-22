b9c8524c6698a30ae1ad327d059f8fc91995f361
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-08-22 11:57:57 -0400

    ..

--- app/Http/Livewire/Page/Edit.php
+++ app/Http/Livewire/Page/Edit.php
@@ -27,9 +27,6 @@ class Edit extends Component
 
     public function render()
     {
-        dd(
-            phpinfo()
-        );
         return view('livewire.page.edit', [
             'questions' => $this->page->questions
         ]);
