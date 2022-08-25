e72a74c31ea99d23cf2a6135276d9960020a35d3
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-08-25 09:58:21 -0400

    added meta tags

--- TODO.md
+++ TODO.md
@@ -2,3 +2,6 @@
 [*] Create thank you page with backlinks to website.
 [*] Create front page
 [] Add _hs_customer_field_of_study to form contidionally
+
+<!-- Tracking template->
+{lpurl}?utm_term={keyword}&utm_campaign={_utmcampaign}&utm_source=adwords&utm_medium=ppc&hsa_acc=5278422239&hsa_cam={campaignid}&hsa_grp={adgroupid}&hsa_ad={creative}&hsa_src={network}&hsa_tgt={targetid}&hsa_kw={keyword}&hsa_mt={matchtype}&hsa_net=adwords&hsa_ver=3
--- resources/views/web/templates/classic-es.blade.php
+++ resources/views/web/templates/classic-es.blade.php
@@ -6,7 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="shortcut icon" href="{{ asset('favicon.jpg') }}" type="image/jpeg">
         <title>{{ ($title ?? '') . ' - ' . config('app.name', 'URBE University') }}</title>
+        <meta name="title" content="{{ $about ?? $title }}">
         <meta name="description" content="{{ $about ?? $title }}">
+        <meta property="og:image" content="{{ asset($cover) }}">
         <!-- Styles -->
         @vite('resources/css/app.css')
 
