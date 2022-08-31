e47e5f56e126e9ad352a26856cf4e88adb134dea
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-08-31 09:55:41 -0400

    implemented loading indicator

--- resources/views/livewire/contact/sync.blade.php
+++ resources/views/livewire/contact/sync.blade.php
@@ -1,11 +1,14 @@
 <div>
-    <x-jet-button wire:click="sync" title="Syncronize all contacts with Hubspot. Existing leads will not be synced.">
+    <x-jet-button wire:click="sync" wire:loading.remove title="Syncronize all contacts with Hubspot. Existing leads will not be synced.">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
             <path fill-rule="evenodd" d="M13.2 2.24a.75.75 0 00.04 1.06l2.1 1.95H6.75a.75.75 0 000 1.5h8.59l-2.1 1.95a.75.75 0 101.02 1.1l3.5-3.25a.75.75 0 000-1.1l-3.5-3.25a.75.75 0 00-1.06.04zm-6.4 8a.75.75 0 00-1.06-.04l-3.5 3.25a.75.75 0 000 1.1l3.5 3.25a.75.75 0 101.02-1.1l-2.1-1.95h8.59a.75.75 0 000-1.5H4.66l2.1-1.95a.75.75 0 00.04-1.06z" clip-rule="evenodd" />
         </svg>
         <span class="ml-2">Sync with Hubspot</span>
     </x-jet-button>
-    <div class="" wire:loading>
-        Sync operation in process...
+    <div class="inline-flex items-center space-x-2" wire:loading>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 animate-spin">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+        <span class="text-sm font-semibold text-slate-600">Syncing contacts with Hubspot...</span>
     </div>
 </div>
