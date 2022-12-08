b43cb097d51668f659ed872939b39e4892858de1
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-12-08 10:43:15 -0500

    Santa icon when christmas

--- resources/views/livewire/announcement/show.blade.php
+++ resources/views/livewire/announcement/show.blade.php
@@ -1,9 +1,12 @@
 <div>
     @if ($event)
         <div class="w-full bg-[#ffd308] z-50">
-            <div class="max-w-7xl mx-auto text-center py-4">
+            <div class="max-w-7xl mx-auto text-center py-4 flex items-center justify-center space-x-3">
                 @if (app()->getLocale() == 'en')
                     <h1 class="capitalize text-xl text-[#073260]">{{ $event->semester }} session starts <span class="font-bold">{{ Carbon\Carbon::parse($event->starts_at)->format('F jS') }}</span></h1>
+                    @if ($event->semester == 'Fall B' || $event->semester == 'Winter A')
+                        <img src="{{ asset('santa-claus.png') }}" alt="" class="w-10">
+                    @endif
                 @else
                 @php
                     Carbon\Carbon::setUTF8(true);
